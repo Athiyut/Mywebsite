9f972cff7339afe17a27f22be23c0bcddf43b2f1
Athiyut <vathiyut@gmail.com>
2016-09-07 19:42:32 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,35 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Password Security</title>
+
+<link href="css/style.css" rel="stylesheet" type="text/css">
+</head>
+
+<body>
+    <div class="container">
+    	<div class="content">
+        	<h1 class="header title">How Secure Is My Password?</h1>
+            <input type="password" placeholder="ENTER PASSWORD" class="password-input">     
+            <div class="sponsor">
+            	<p class="sponsor_text">Sponsored by Programming inc.</p></div>
+                <div class="social">
+                	<div class="social__twitter">                   	
+                    	<a href="https://twitter.com/PlusHoliday" target="_blank"> <img src="image/twiter.jpg"></a>
+                    </div>
+                    <div class="social__facebook-wrapper">
+                    	<a href="https://www.facebook.com/plusholiday.travel"><img src="image/facebook.png"></a>
+                    </div>
+                </div>
+                <div class="footer">
+                	<p>Sponsored by testing</p>
+                 
+                </div>
+            </div> 
+   
+        </div>    
+        </div>
+    </div>
+ </body>
+</html>
